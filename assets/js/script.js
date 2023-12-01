f913fa2614fdca26ba88f8c6cef12d02581c1b7b
Zach <zdgemmill@gmail.com>
2023-11-30 20:28:27 -0700

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var submitBtn = $("button");
 var ingredient = $("#ingredient");
+var results = $("#results");
 
 //event listener on submit button
 
@@ -11,7 +12,7 @@ $(submitBtn).on('click', function (event) {
     //might need to change to search by ingredient and not drink name
     var name = userIngredients;
     var apiKey = '7t2sHRZWaFF1UKnhMSLCeQ==iC5BhRw1veT5gYny';
-    fetch(`https://api.api-ninjas.com/v1/cocktail?name=${name}`, {
+    fetch(`https://api.api-ninjas.com/v1/cocktail?ingredients=${name}`, {
         method: 'GET',
         headers: {
             'X-Api-Key': apiKey,
@@ -32,53 +33,31 @@ $(submitBtn).on('click', function (event) {
             console.error('There was a problem with the fetch operation:', error);
         });
 
-    listOfI = []
-
-    // for (let i = 0; i < data.length; i++) {
-    //     var ingredientLi = data[i].ingredients()
-    //     ingredientLi.push(listOfI)
 
 
-    // }
-    // console.log(listOfI)
 
 
 });
 
 
 
-// function menuBar(x) {
-//     x.classList.toggle("change");
-// }
-
-
-//this is comented out for testing
-// //fetching cocktail data
-// var name = userIngredients;
-// var apiKey = '7t2sHRZWaFF1UKnhMSLCeQ==iC5BhRw1veT5gYny';
-
-// fetch(`https://api.api-ninjas.com/v1/cocktail?name=${name}`, {
-//     method: 'GET',
-//     headers: {
-//         'X-Api-Key': apiKey,
-//         'Content-Type': 'application/json'
-//     }
-// })
-//     .then(response => {
-//         if (!response.ok) {
-//             throw new Error('Network response was not ok.');
-//         }
-//         return response.json();
-//     })
-//     .then(data => {
-//         console.log(data);
-//     })
-//     .catch(error => {
-//         console.error('There was a problem with the fetch operation:', error);
-//     });
-
-
-// this is a change
-
-
-//window.onload = fetchData;
+fetch(`https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg/preview`, {
+    method: 'GET',
+    headers: {
+        'X-Api-Key': apiKey,
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok.');
+        }
+        return response.json();
+    })
+    //for loop for looping over data objects and "trying" to console log the ingredients
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
